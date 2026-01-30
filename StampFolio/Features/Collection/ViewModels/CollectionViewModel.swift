65855c4f36d9bb54511168070429cc86d23ca8ad
babalicious-io <baba@babalicious.io>
2026-01-30 22:49:38 +0100

    menu change

--- StampFolio/Features/Collection/ViewModels/CollectionViewModel.swift
+++ StampFolio/Features/Collection/ViewModels/CollectionViewModel.swift
@@ -22,12 +22,12 @@ enum SortOption: String, CaseIterable, Codable {
     
     var displayName: String {
         switch self {
-        case .stampAscending: return "Stamp # (ascending)"
-        case .stampDescending: return "Stamp # (descending)"
+        case .stampAscending: return "Stamp #"
+        case .stampDescending: return "Stamp #"
         case .artistAZ: return "Artist (A-Z)"
         case .artistZA: return "Artist (Z-A)"
-        case .balanceAscending: return "Balance (ascending)"
-        case .balanceDescending: return "Balance (descending)"
+        case .balanceAscending: return "Balance"
+        case .balanceDescending: return "Balance"
         case .walletAZ: return "Wallet (A-Z)"
         case .walletZA: return "Wallet (Z-A)"
         }
@@ -38,7 +38,7 @@ enum SortOption: String, CaseIterable, Codable {
         case .stampAscending, .balanceAscending: return "arrow.up"
         case .stampDescending, .balanceDescending: return "arrow.down"
         case .artistAZ, .walletAZ: return "textformat.abc"
-        case .artistZA, .walletZA: return "textformat.abc"
+        case .artistZA, .walletZA: return "textformat.zyx"
         }
     }
 }
