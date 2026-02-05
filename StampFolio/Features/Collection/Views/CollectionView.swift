1a1019e68c8a67179fd0d8af864d95ca1e14f0f8
babalicious-io <baba@babalicious.io>
2026-02-05 16:32:37 +0100

    padding

--- StampFolio/Features/Collection/Views/CollectionView.swift
+++ StampFolio/Features/Collection/Views/CollectionView.swift
@@ -138,8 +138,8 @@ struct CollectionView: View {
                         .stroke(Color(uiColor: .separator).opacity(0.3), lineWidth: 0.5)
                 )
                 .shadow(color: .black.opacity(0.2), radius: 12, y: 4)
-                .padding(.top, 0)
-                .padding(.trailing, 16)
+                .padding(.top, horizontalSizeClass == .compact ? 8 : 16)
+                .padding(.trailing, 8)
                 .transition(.scale(scale: 0.01, anchor: .topTrailing).combined(with: .opacity))
                 .zIndex(1000)
         }
