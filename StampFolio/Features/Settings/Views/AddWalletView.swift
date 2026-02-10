a236ba1d44fc22a6387cf51314313761bccb4a5e
babalicious-io <baba@babalicious.io>
2026-02-10 14:10:22 +0100

    size

--- StampFolio/Features/Settings/Views/AddWalletView.swift
+++ StampFolio/Features/Settings/Views/AddWalletView.swift
@@ -50,7 +50,7 @@ struct AddWalletView: View {
                         } label: {
                             Image(systemName: "qrcode")
                                 .foregroundStyle(appColorScheme.primary)
-                                .font(.system(size: 22))
+                                .font(.system(size: 24))
                         }
                         .buttonStyle(.plain)
                         .accessibilityLabel("Scan QR code")
