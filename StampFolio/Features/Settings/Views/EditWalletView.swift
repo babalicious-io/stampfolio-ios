a9310c4633e1058e913fca1cffd07210e5b19530
babalicious-io <baba@babalicious.io>
2026-02-10 12:33:42 +0100

    Update circle size in EditWalletView for improved visual consistency with wallet color options.

--- StampFolio/Features/Settings/Views/EditWalletView.swift
+++ StampFolio/Features/Settings/Views/EditWalletView.swift
@@ -62,7 +62,7 @@ struct EditWalletView: View {
                                 ZStack {
                                     Circle()
                                         .fill(color.color)
-                                        .frame(width: 24, height: 24)
+                                        .frame(width: 28, height: 28)
                                     
                                     if selectedColor == color {
                                         Image(systemName: "checkmark")
