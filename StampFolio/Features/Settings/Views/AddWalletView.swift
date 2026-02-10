a02e2fe52e33ffbcb94975fc10dc315ef16e0c37
babalicious-io <baba@babalicious.io>
2026-02-10 13:29:10 +0100

    spacing

--- StampFolio/Features/Settings/Views/AddWalletView.swift
+++ StampFolio/Features/Settings/Views/AddWalletView.swift
@@ -80,7 +80,7 @@ struct AddWalletView: View {
                 
                 // Wallet Color Section
                 Section {
-                    HStack(spacing: 16) {
+                    HStack(spacing: 24) {
                         ForEach(WalletColor.allCases) { color in
                             Button {
                                 withAnimation(.easeInOut(duration: 0.2)) {
